2960c2d2fdc294629d90028f128eda16e364df84
matishen7 <ishenbaev7@gmail.com>
2024-09-01 14:20:22 -0500

    Job model update

--- Models/Job.cs
+++ Models/Job.cs
@@ -1,15 +1,28 @@
-﻿using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+﻿using System.ComponentModel.DataAnnotations;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace JobSearchAppBackend.Models
 {
     public class Job
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Job title is required.")]
+        [StringLength(100, ErrorMessage = "Job title cannot exceed 100 characters.")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Job description is required.")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Location is required.")]
         public string Location { get; set; }
-        public DateTime PostedDate { get; set; }
-        public int CompanyId { get; set; }
+
+        public DateTime PostedDate { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Company ID is required.")]
+        public int CompanyId { get; set; } // Foreign key reference to the Company
+
+        // Navigation property
         public Company Company { get; set; }
     }
 }
