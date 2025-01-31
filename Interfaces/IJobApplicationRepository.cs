6f42847ebba641afe6d0f7839d1ef80423fdc077
matishen7 <ishenbaev7@gmail.com>
2025-01-31 15:43:55 -0600

    add IJobApplicationService

---
+++ Interfaces/IJobApplicationRepository.cs
@@ -0,0 +1,13 @@
+﻿using JobSearchAppBackend.Models;
+
+namespace JobSearchAppBackend.Interfaces
+{
+    public interface IJobApplicationRepository
+    {
+        Task<List<JobApplication>> GetAllJobApplicationsAsync();
+        Task<JobApplication> GetJobApplicationByIdAsync(int jobApplicationId);
+        Task<int> AddJobApplicationAsync(JobApplication jobApplication);
+        Task UpdateJobApplicationAsync(JobApplication jobApplication);
+        Task DeleteJobApplicationAsync(int jobApplicationId);
+    }
+}
---
+++ Interfaces/IJobApplicationService.cs
@@ -0,0 +1,14 @@
+﻿using JobSearchAppBackend.DTOs;
+using JobSearchAppBackend.Models;
+
+namespace JobSearchAppBackend.Interfaces
+{
+    public interface IJobApplicationService
+    {
+        Task<List<JobApplicationDto>> GetAllJobApplicationsAsync();
+        Task<JobApplicationDto> GetJobApplicationByIdAsync(int jobApplicationId);
+        Task<int> AddJobApplicationAsync(JobApplicationCreateDto jobApplication);
+        Task UpdateJobApplicationAsync(JobApplicationCreateDto jobApplication);
+        Task DeleteJobApplicationAsync(int jobApplicationId);
+    }
+}
